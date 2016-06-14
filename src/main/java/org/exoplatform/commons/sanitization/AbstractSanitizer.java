0d1ecb79b218789d6070a116671d237852aa0831
Khemais <kmenzli@exoplatform.com>
2016-06-14 17:32:38 +0100

    Initial commit

---
+++ src/main/java/org/exoplatform/commons/sanitization/AbstractSanitizer.java
@@ -0,0 +1,41 @@
+package org.exoplatform.commons.sanitization;
+
+import org.owasp.validator.html.*;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+
+
+/**
+ * Created by kmenzli on 02/02/16.
+ */
+public abstract class AbstractSanitizer {
+    private static final Log LOG = ExoLogger.getLogger(AbstractSanitizer.class);
+    private static final String RULES = "social-rules-1.0.0-SNAPSHOT";
+
+    private static Policy policy;
+    private static AntiSamy antiSamy;
+
+    protected static AntiSamy getAntiSamy() throws PolicyException  {
+        if (antiSamy == null) {
+            policy = getPolicy(RULES);
+            antiSamy = new AntiSamy();
+        }
+        return antiSamy;
+
+    }
+
+    protected static String sanitize(String input) {
+        CleanResults cr;
+        try {
+            cr = getAntiSamy().scan(input, policy);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return cr.getCleanHTML();
+    }
+
+    private static Policy getPolicy(String name) throws PolicyException {
+        Policy policy = Policy.getInstance(Policy.class.getResourceAsStream("/META-INF/antisamy/" + name + ".xml"));
+        return policy;
+    }
+}
---
+++ src/main/java/org/exoplatform/commons/sanitization/social/SocialSanitizer.java
@@ -0,0 +1,12 @@
+package org.exoplatform.commons.sanitization.social;
+
+import org.exoplatform.commons.sanitization.AbstractSanitizer;
+import org.owasp.validator.html.Policy;
+import org.owasp.validator.html.PolicyException;
+
+/**
+ * Created by kmenzli on 02/02/16.
+ */
+public class SocialSanitizer extends AbstractSanitizer {
+
+}
