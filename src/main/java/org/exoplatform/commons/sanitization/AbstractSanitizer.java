6a982687d060a834553a3108f865ef429991c09f
Khemais <kmenzli@exoplatform.com>
2016-06-20 09:48:25 +0100

    Message 1 : add a new method called merge

--- src/main/java/org/exoplatform/commons/sanitization/AbstractSanitizer.java
+++ src/main/java/org/exoplatform/commons/sanitization/AbstractSanitizer.java
@@ -38,4 +38,7 @@ public abstract class AbstractSanitizer {
         Policy policy = Policy.getInstance(Policy.class.getResourceAsStream("/META-INF/antisamy/" + name + ".xml"));
         return policy;
     }
+    private static void merge (String branch) throws Exception {
+        System.out.println("The branch name is : "+branch);
+    }
 }
